336316a5e1fee26c4b0a89df81095a2e834ccb04
edward salcido <edwardromero51609@gmail.com>
2021-03-08 17:53:56 -0600

    first commit

---
+++ task 3.py
@@ -0,0 +1,16 @@
+print("hello_USERNAME")
+
+numbers = range(101)
+numbers
+range(0, 101)
+for number in numbers :
+    if number % 2 == 0 :
+        print ( number )
+
+print(max_num_in_list(list))
+    max = list[0]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+print(max_num_in_list[1, 2, -8, 0]))
